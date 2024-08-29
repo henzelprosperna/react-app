cb33eeb19ea2c6bd957146f051355c4e9a73e9f9
henzel@prosperna.com <henzel@prosperna.com>
2024-08-29 11:24:52 +0800

    Edited skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,12 +5,9 @@ function Skills() {
     <section id="skills" className="p-8 bg-gray-800">
       <h2 className="text-3xl font-semibold mb-4">Skills</h2>
       <ul className="list-disc pl-5 space-y-2">
-        <li>Programming (Java, .NET, Node.js)</li>
-        <li>Web Development</li>
-        <li>IT Infrastructure/System Administration</li>
+        <li>System Administration</li>
         <li>Software and Hardware Troubleshooting</li>
         <li>Basic Networking</li>
-        <li>Back-end Development</li>
         <li>Technical Support</li>
         <li>Customer Service</li>
       </ul>
